2f4b8a482b4e95a820ac083886e8524d144d7fcf
DENSOFTDEV <wakahiad@gmail.com>
2020-03-23 11:30:23 +0300

    access token  request edited

--- access_token.php
+++ access_token.php
@@ -10,9 +10,10 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $url);
 $credentials = base64_encode('Q7iPSZGcE22okEbWet9A91abFwGZtOpp:EhkCgApwccdizVC1');
 curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); //setting a custom header
-curl_setopt($curl, CURLOPT_HEADER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $curl_response = curl_exec($curl);
 
-echo json_decode($curl_response);
+echo json_decode($curl_response)->access_token;
